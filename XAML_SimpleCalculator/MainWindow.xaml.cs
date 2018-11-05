3241b330cafc9e5091539bfe28210b0f819a3493
Kevin <stoutkev3@gmail.com>
2018-11-04 21:53:20 -0500

    finished converting my WinForms calculator to XAML

--- XAML_SimpleCalculator/MainWindow.xaml.cs
+++ XAML_SimpleCalculator/MainWindow.xaml.cs
@@ -20,9 +20,78 @@ namespace XAML_SimpleCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            if ((textBox_Result.Text == "0") || (isOperationPerformed))
+            {
+                textBox_Result.Clear();
+            }
+
+            Button button = (Button)sender;
+            if (button.Content.ToString() == ".")
+            {
+                if (!textBox_Result.Text.Contains("."))
+                {
+                    textBox_Result.Text = textBox_Result.Text + button.Content;
+                }
+            }
+            else
+            {
+                textBox_Result.Text = textBox_Result.Text + button.Content;
+            }
+
+            isOperationPerformed = false;
+        }
+
+        private void operator_click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerformed = button.Content.ToString();
+            resultValue = Double.Parse(textBox_Result.Text);
+            labelCurrentOperation.Content = resultValue + " " + operationPerformed;
+            isOperationPerformed = true;
+        }
+
+        private void clear_entry(object sender, RoutedEventArgs e)
+        {
+            textBox_Result.Text = "0";
+        }
+
+        private void clear_All(object sender, RoutedEventArgs e)
+        {
+            textBox_Result.Text = "0";
+            resultValue = 0;
+            labelCurrentOperation.Content = "";
+        }
+
+        private void equals_click(object sender, RoutedEventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    textBox_Result.Text = (resultValue + Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_Result.Text = (resultValue - Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_Result.Text = (resultValue * Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_Result.Text = (resultValue / Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
